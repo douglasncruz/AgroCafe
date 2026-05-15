815c54a108b8c1eda0d9f1751ec0bba589b255eb
douglasncruz <douglasncruz@gmail.com>
2026-05-15 13:46:39 -0300

    Auto-deploy: .git

--- agrocafe/frontend/src/app/layout.tsx
+++ agrocafe/frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { HarvestProvider } from "@/context/HarvestContext";
 import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
@@ -25,8 +26,10 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
-          <Toaster position="top-right" richColors />
+          <HarvestProvider>
+            {children}
+            <Toaster position="top-right" richColors />
+          </HarvestProvider>
         </ThemeProvider>
       </body>
     </html>
