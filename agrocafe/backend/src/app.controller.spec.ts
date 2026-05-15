b75bc872c4e85b9376d22b2df2fe66f8a9b14a26
douglasncruz <douglasncruz@gmail.com>
2026-05-15 14:52:33 -0300

    Auto-deploy: app.controller.spec.ts

--- agrocafe/backend/src/app.controller.spec.ts
+++ agrocafe/backend/src/app.controller.spec.ts
@@ -23,7 +23,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe('AgroCerradoCafé API is running!');
     });
   });
 });
