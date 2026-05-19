605e3a05f0f6cff418140d86b2fcb8cdda4f7f90
douglasncruz <douglasncruz@gmail.com>
2026-05-19 08:54:53 -0300

    Auto-deploy: harvest-validation.service.ts

--- agrocafe/backend/src/expenses/expenses.module.ts
+++ agrocafe/backend/src/expenses/expenses.module.ts
@@ -4,9 +4,13 @@ import { ExpensesService } from './expenses.service';
 import { ExpensesController } from './expenses.controller';
 import { Expense } from './entities/expense.entity';
 import { Farm } from '../farms/entities/farm.entity';
+import { HarvestsModule } from '../harvests/harvests.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Expense, Farm])],
+  imports: [
+    TypeOrmModule.forFeature([Expense, Farm]),
+    HarvestsModule,
+  ],
   controllers: [ExpensesController],
   providers: [ExpensesService],
 })
--- agrocafe/backend/src/revenues/revenues.module.ts
+++ agrocafe/backend/src/revenues/revenues.module.ts
@@ -4,9 +4,13 @@ import { RevenuesService } from './revenues.service';
 import { RevenuesController } from './revenues.controller';
 import { Revenue } from './entities/revenue.entity';
 import { Farm } from '../farms/entities/farm.entity';
+import { HarvestsModule } from '../harvests/harvests.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Revenue, Farm])],
+  imports: [
+    TypeOrmModule.forFeature([Revenue, Farm]),
+    HarvestsModule,
+  ],
   controllers: [RevenuesController],
   providers: [RevenuesService],
 })
