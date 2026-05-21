b96cd469a7bbe8f7ac931330f4b81aef5ef458de
douglasncruz <douglasncruz@gmail.com>
2026-05-21 13:53:51 -0300

    Update automatizado e validacao de build

--- agrocafe/backend/src/expenses/expenses.controller.ts
+++ agrocafe/backend/src/expenses/expenses.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Body, UseGuards, UseInterceptors, UploadedFile, Req, Delete, Param } from '@nestjs/common';
+﻿import { Controller, Get, Post, Put, Body, UseGuards, UseInterceptors, UploadedFile, Req, Delete, Param } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
